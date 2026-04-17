421365b7db34b2ca4d32d25018b6108c1a08ffef
Your Name <you@example.com>
2026-04-17 21:05:54 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -45,7 +45,7 @@ int is_equal(void* key1, void* key2){
 // Inicialice el índice current a -1.
 
 HashMap * createMap(long capacity) {
-    HashMap *new = (HashMap *)malloc(sizzof(Hashmap));
+    HashMap *new = (HashMap *)malloc(sizeof(HashMap));
     if (new == NULL) exit(EXIT_FAILURE);
     new->buckets = (Pair **)malloc(sizeof(Pair *) * 7);
     new->capacity = 7;
