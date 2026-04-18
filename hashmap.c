b68b287e194001841f1e0ff1b2bf9d59b606e63f
Your Name <you@example.com>
2026-04-18 00:31:40 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -159,13 +159,14 @@ void enlarge(HashMap * map) {
     Pair **new_buckets = (Pair **)malloc(sizeof(Pair *) * map->capacity * 2);
     map->buckets = new_buckets;
     map->size = 0;
+    long old_size = map->capacity;
+    map->capacity = map->capacity * 2;
 
-    for (long i = 0 ; i < map->capacity ; i++)
+    for (long i = 0 ; i < old_capacity ; i++)
     {
         if (old_buckets[i] == NULL || old_buckets[i]->key == NULL) continue;
         insertMap(map, old_buckets[i]->key, old_buckets[i]->value);
     }
-    map->capacity = map->capacity * 2;
 }
 
 
